46c46acbd70a607c323bdc3f3363f59e4f836523
ka3s <ka3s1mya@gmail.com>
2024-06-26 19:51:43 -0400

    Added more sections and questions

--- main.py
+++ main.py
@@ -25,31 +25,58 @@ study_qna = [
     {"question": "_____ ________ is the process of ensuring that assets stored in the cloud are properly configured, or set up correctly, and access to those assets is limited to authorized users. The cloud is a network made up of a collection of servers or computers that store resources and data in remote physical locations known as data centers that can be accessed via the internet. Cloud security is a growing subfield of cybersecurity that specifically focuses on the protection of data, applications, and infrastructure in the cloud.\n What is the answer to this question?: ", "answer": "cloud security"},
     #question 9
     {"question": "___________ is a process that can be used to create a specific set of instructions for a computer to execute tasks. These tasks can include: \n *Automation of repetitive tasks (e.g., searching a list of malicious domains)\n *Reviewing web traffic\n *Alerting suspicious activity\n What is the answer to this question?: ", "answer": "programming"},
-    #part 4 Question 1
-    {"question": "________ ________ is a software program used to prevent, detect, and eliminate malware and viruses. It is also called anti-malware.\n What is the answer to this question?: ", "answer": "antivirus software"},
-    #p4 Question 2
-    {"question": "________: An organized collection of information or data.\n Fill in the blank: ", "answer": "database"},
-    #p4 Question 3
-    {"question": "____ _____: A specific piece of information. \n Fill in the blank: ", "answer": "data point"},
-    #p4 Question 4
-    {"question": "_________ _________ ______: An Application that monitors system activity and alerts on possible intrusions.\n Fill in the blank: ", "answer": "Intrusion Detection System"},
-    #p4 Question 5
-    {"question": "__________ makes data unreadable and difficult to decode for an unauthorized user; its main goal is to ensure confidentiality of private data.\n What is the answer to this question?: ", "answer": "encryption"},
-    #p4 Question 6
-    {"question": "___________ _______, is the act of participating in a simulated attack that helps identify vulnerabilities in a system, network, website, application, and processes. It is a thorough risk assesment that can evaluate and identify external and internal threats as well as weaknesses.\n What is the answer to this Question?: ", "answer": "Penetration Testing"},
-    #p4 Question 7
-    {"question": "_____ is an open source operating system. \n What is the answer to this question?: ", "answer": "linux"},
-    #p4 Question 8
-    {"question": "A ___ is a record of events that occur within an organization's systems\n What is the answer to this question?: ", "answer": "log"},
-    #p4 Question 9
-    {"question": "_______ ________ ________: A tool designed to capture and analyze data traffic within a network\n Fill in the blank: ", "answer": "Network Protocol Analyzer"},
-    #p4 Question 10
-    {"question": "_______ __ _______: Is a sequence outlining the order of data that must be preserved from first to last\n Fill in the blank: ", "answer": "Order of Volatility"},
-    #4q11
-    {"question": "________ ___________ ___ __________: Is an application that collects and analyzes log data to monitor critical activities in an ogranization\n Fill in the blank: ", "answer": "Security Information And Management"},
-    #4q12
-    {"question": "___: Is a query language used to create, interact with, and request information from a database.\n Fill in the blank: ", "answer": "SQL"}
+]
 
+study_qnaTwo = [
+    #p2q1
+    {"question": "________ ________ is a software program used to prevent, detect, and eliminate malware and viruses. It is also called anti-malware.\n What is the answer to this question?: ", "answer": "antivirus software"},
+    #p2q2
+    {"question": "________: An organized collection of information or data.\n Fill in the blank: ", "answer": "database"},
+    #p2q3
+    {"question": "____ _____: A specific piece of information. \n Fill in the blank: ", "answer": "data point"},
+    #p2q4
+    {"question": "_________ _________ ______: An Application that monitors system activity and alerts on possible intrusions.\n Fill in the blank: ", "answer": "Intrusion Detection System"},
+    #p2q5
+    {"question": "__________ makes data unreadable and difficult to decode for an unauthorized user; its main goal is to ensure confidentiality of private data.\n What is the answer to this question?: ", "answer": "encryption"},
+    #p2q6
+    {"question": "___________ _______, is the act of participating in a simulated attack that helps identify vulnerabilities in a system, network, website, application, and processes. It is a thorough risk assesment that can evaluate and identify external and internal threats as well as weaknesses.\n What is the answer to this Question?: ", "answer": "Penetration Testing"},
+    #p2q7
+    {"question": "_____ is an open source operating system. \n What is the answer to this question?: ", "answer": "linux"},
+    #p2q8
+    {"question": "A ___ is a record of events that occur within an organization's systems\n What is the answer to this question?: ", "answer": "log"},
+    #p2q9
+    {"question": "_______ ________ ________: A tool designed to capture and analyze data traffic within a network\n Fill in the blank: ", "answer": "Network Protocol Analyzer"},
+    #p2q10
+    {"question": "_______ __ _______: Is a sequence outlining the order of data that must be preserved from first to last\n Fill in the blank: ", "answer": "Order of Volatility"},
+    #2q11
+    {"question": "________ ___________ ___ __________: Is an application that collects and analyzes log data to monitor critical activities in an ogranization\n Fill in the blank: ", "answer": "Security Information And Management"},
+    #2q12
+    {"question": "___: Is a query language used to create, interact with, and request information from a database.\n Fill in the blank: ", "answer": "SQL"}
+]
+
+#CISSP DOMAINS
+
+study_domains = [
+    #p3q1
+    {"question": "What is the first domain?: ", "answer": "security and risk management"},
+    #p3q2
+    {"question": "What is the second domain?: ", "answer": "asset security"},
+    #p3q3
+    {"question": "What is the third domain?: ", "answer": "security architecture and engineering"},
+    #p3q4
+    {"question": "What is the fourth domain?: ", "answer": "communication and network security"},
+    #p3q5
+    {"question": "What is the fifth domain?: ", "answer": "identity and access management"},
+    #p3q6
+    {"question": "What is the sixth domain?: ", "answer": "security assessment and testing"},
+    #p3q7
+    {"question": "What is the seventh domain?: ", "answer": "security operations"},
+    #p3q8
+    {"question": "What is the eighth domain?: ", "answer": "software development security"}
+]
+
+study_domain_definitions = [
+    #p4q1
 ]
 
 #calling the function here, returning results per question answered
